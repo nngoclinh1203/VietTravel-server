7d328d44350734cfe9231bae0def4f6efc6fa5ff
Nguyễn Ngọc Linh <100554756+nngoclinh1203@users.noreply.github.com>
2024-05-18 16:45:18 +0700

    get comment user

--- src/api/controllers/comment.js
+++ src/api/controllers/comment.js
@@ -68,6 +68,26 @@ exports.addComment = async (req, res) => {
     }
 }
 
+//api/comments/get-comments/:username
+exports.getCommentsHistoricalSite = async (req, res) => {
+    try {
+        const username = req.params.username;
+            
+        const user = await User.findOne({ username: username });
+        if (!user) return res.status(404).json({ message: "User not found!" });
+
+        const comments = await Comment.find({ userId: user.userId });
+        if (!comments) return res.status(404).json({ message: "Comments not found!" });
+
+        res.status(200).json(comments);
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: err.message });
+    }
+}
+
 //api/comments/get-comments/:historySiteId
 exports.getCommentsHistoricalSite = async (req, res) => {
     try {
